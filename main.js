f4a8149419f576e9d2d7aac6df9c43ead387ed4c
Loïc <fuentes.loic@gmail.com>
2017-01-23 16:47:31 +0100

    install leaflet and leaflet via npm

--- main.js
+++ main.js
@@ -3,33 +3,24 @@
 // init map
 
 const map = L.map('map', {
-  center: [50, -100],
-  zoom: 3,
+  center: [0, 0],
+  zoom: 2,
   layers: [
     L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibG9pY2Z1ZW50ZXMiLCJhIjoiY2l4cWFvanBwMDAyNzJ3cGJkNnRmanp0diJ9.Wt3BUvjiwJwbfMGHNQFQxg")
   ]
 });
 
-const giveData = new Worker("dataWorker.js");
-
 // marker web Worker
 
-let federationLayers = {};
-let infoCount = 0;
-giveData.onmessage = (informations) => {
-  let [lat, lng, federation, city, length, country, countrySelected] = informations.data;
-  infoCount++;
-  // federation key is not registered yet
-  if (country === countrySelected) {
-    if (!(federation in federationLayers)) {
-      federationLayers[federation] = L.layerGroup();
-    }
-    // add the layer to the current federation
-    federationLayers[federation].addLayer(L.marker([lat, lng]).bindPopup(`${city} in ${federation}`));
-  }
-  // info counter
-  if(infoCount === length) {
-    L.control.layers(federationLayers).addTo(map);
-    infoCount = 0;
-  }
-};
+var markers1 = L.markerClusterGroup();
+var markers2 = L.markerClusterGroup();
+var markers3 = L.markerClusterGroup();
+var markers4 = L.markerClusterGroup();
+var markers5 = L.markerClusterGroup();
+markers1.addLayer(L.marker([40,-100]));
+markers2.addLayer(L.marker([50,-100]));
+markers3.addLayer(L.marker([40,-110]));
+markers4.addLayer(L.marker([40,-150]));
+markers4.addLayer(L.marker([40,-110]));
+// ... Add more layers ...
+map.addLayer(markers1).addTo(map);
