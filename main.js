46db39ab82cfcc7b6c2f22685f505de1b5683c5a
Loïc <fuentes.loic@gmail.com>
2017-01-23 15:42:42 +0100

    remove country web worker and header

--- main.js
+++ main.js
@@ -10,22 +10,7 @@ const map = L.map('map', {
   ]
 });
 
-const countriesList = new Worker("countriesWorker.js");
 const giveData = new Worker("dataWorker.js");
-
-// countriesList web worker
-
-const selectElement = document.getElementById("selectCountry");
-countriesList.onmessage = (countries) => {
-  let [code, name] = countries.data;
-  let optionElement = document.createElement("option");
-  optionElement.setAttribute("value", code);
-  let insertCode = document.createTextNode(name);
-  optionElement.appendChild(insertCode);
-  selectElement.insertBefore(optionElement, null);
-};
-
-countriesList.postMessage("send countries");
 
 // marker web Worker
 
@@ -48,26 +33,3 @@ giveData.onmessage = (informations) => {
     infoCount = 0;
   }
 };
-
-selectCountry = _ => {
-  federationLayers = {};
-  let select = document.getElementById("selectCountry");
-  let countryValue = select.options[select.selectedIndex].value;
-
-  giveData.postMessage(countryValue);
-};
-
-// return zoom level
-zoomLev = map.getZoom();
-console.log(zoomLev);
-map.on("zoomend", _ => {
-  zoomLev = map.getZoom();
-  console.log(zoomLev);
-  if (zoomLev <= 3){
-    console.info("entre 0 et 3 inclus");
-  } else if (zoomLev > 3 && zoomLev <= 7) {
-    console.info("entre 4 et 7 inclus");
-  } else {
-    console.info("8 et +");
-  }
-});
